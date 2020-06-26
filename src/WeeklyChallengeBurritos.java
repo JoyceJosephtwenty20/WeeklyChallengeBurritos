3b9b34ec7b617c18b1eddb87d0d418181eee619b
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-26 15:53:09 -0400

    SecondCommit

--- src/WeeklyChallengeBurritos.java
+++ src/WeeklyChallengeBurritos.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import java.util.Random;
@@ -6,9 +7,8 @@ import java.util.Random;
 class WeeklyChallengeBurritos {
     public static void main(String[] args) {
 
-        Random rand = new Random();
 
-        String[] table = new String[9];
+        Random rand = new Random();
 
         ArrayList<String> rice = new ArrayList<String>();
 
@@ -29,148 +29,111 @@ class WeeklyChallengeBurritos {
         meat.add("all");
 
         ArrayList<String> beans = new ArrayList<String>();
-
-        beans.add("pinto");
-        beans.add("black");
+        beans.add("pinto beans");
+        beans.add("black beans");
         beans.add("none");
         beans.add("all");
 
         ArrayList<String> salsa = new ArrayList<String>();
-
-        salsa.add("mild");
-        salsa.add("medium");
-        salsa.add("hot");
+        salsa.add("mild salsa");
+        salsa.add("medium salsa");
+        salsa.add("hot salsa");
         salsa.add("none");
         salsa.add("all");
 
         ArrayList<String> veggies = new ArrayList<String>();
-
         veggies.add("lettuce");
         veggies.add("fajita veggies");
         veggies.add("none");
         veggies.add("all");
 
-        ArrayList<String> cheese = new ArrayList<String>();
-
-        cheese.add("yes");
-        cheese.add("no");
-
-        ArrayList<String> guac = new ArrayList<String>();
-
-        guac.add("yes");
-        guac.add("no");
-
-        ArrayList<String> queso = new ArrayList<String>();
-
-        queso.add("yes");
-        queso.add("no");
-
         ArrayList<String> sourCream = new ArrayList<String>();
-
         sourCream.add("yes");
         sourCream.add("no");
 
-        double ricePrice;
+        ArrayList<String> guac = new ArrayList<String>();
+        guac.add("yes");
+        guac.add("no");
+
+        // to get double precision
+
+        DecimalFormat dec = new DecimalFormat("#0.00");
 
         String randRice;
         String randMeat;
         String randBean;
         String randSalsa;
         String randVegge;
-        String randCheese;
-        String randGuac;
-        String randQueso;
         String randSourCream;
+        String randGuac;
 
-        // prints 10 burritios
-        for (int i = 1; i < 26; i++) {
+        for (int i = 1; i <= 25; i++) {
             double total = 3.00;
             randRice = rice.get(rand.nextInt(rice.size()));
             randMeat = meat.get(rand.nextInt(meat.size()));
             randBean = beans.get(rand.nextInt(beans.size()));
             randSalsa = salsa.get(rand.nextInt(salsa.size()));
             randVegge = veggies.get(rand.nextInt(veggies.size()));
-            randCheese = cheese.get(rand.nextInt(cheese.size()));
-            randGuac = guac.get(rand.nextInt(guac.size()));
-            randQueso = queso.get(rand.nextInt(queso.size()));
             randSourCream = sourCream.get(rand.nextInt(sourCream.size()));
+            randGuac = guac.get(rand.nextInt(guac.size()));
 
             // if none
             if (randRice.equalsIgnoreCase("none")){
-                randRice = "no rice,";
-            }else if (randRice.equalsIgnoreCase("all")){
-                randRice = "brown rice, white rice,";
-                total += 1.00;
-            }else{
-                randRice = randRice;
-                total +=0.50;
+                randRice = "no rice";
             }
             if (randMeat.equalsIgnoreCase("none")){
-                randRice = "no meat,";
-            }else if (randMeat.equalsIgnoreCase("all")) {
-                randMeat = "chicken, steak, carnidas,";
-                total += 1.50;
-            }else {
-                randMeat = randMeat;
-                total += 0.50;
+                randMeat = "no meat";
             }
             if (randBean.equalsIgnoreCase("none")){
-                randRice = "no bean,";
-            }else if (randBean.equalsIgnoreCase("all")) {
-                randBean = "pinto, black,";
-                total += 1.00;
-            }else{
-                randBean = randBean;
-                total +=0.5;
+                randBean = "no bean";
             }
-            if (randSalsa.equalsIgnoreCase("none")) {
-                randRice = "no salsa,";
-            }else if (randSalsa.equalsIgnoreCase("all")) {
-                randSalsa = "mild, medium, hot";
-                total += 1.50;
-            }else {
-                randSalsa = randVegge;
-                total +=0.50;
-
+            if (randSalsa.equalsIgnoreCase("none")){
+                randSalsa = "no salsa";
             }
             if (randVegge.equalsIgnoreCase("none")){
-                randVegge = "no veggies,";
-            }else if (randVegge.equalsIgnoreCase("all")) {
-                randVegge = "lettuce, fajita veggies";
-                total += 1.00;
-            } else {
-                randVegge = randVegge;
-                total += 0.50;
-            }
-            if (randCheese.equalsIgnoreCase("no")) {
-                randCheese = "no cheese";
-            }else {
-                randCheese = "cheese";
-                total += 0.50;
-            }
-            if(randGuac.equalsIgnoreCase("no")) {
-                randGuac = "no guac";
-            }else {
-                randGuac = "guac";
-                total += 0.50;
-            }
-            if (randQueso.equalsIgnoreCase("no")) {
-                randQueso = "no queso";
-            }else {
-                randQueso = "queso";
-                total += 0.50;
+                randVegge = "no veggies";
             }
             if (randSourCream.equalsIgnoreCase("no")){
-                randSourCream = "no sour cream";
-            }else {
-                randSourCream = "sour cream";
-                total += 0.50;
+                randSourCream = "";
+            }
+            if (randGuac.equalsIgnoreCase("no")){
+                randGuac = "";
             }
 
-            System.out.println("Burrito " + i + " " + randRice + " " + randMeat +
-                    ", " + randBean + ", " + randSalsa + " " + randVegge + " "+ randCheese + " " + randGuac + " " + randQueso +
-                    " " + randSourCream+ " $" + total);
+            // if all
+            if (randRice.equalsIgnoreCase("all")){
+                randRice = "all rice";
+                total += .50;
+            }
+            if (randMeat.equalsIgnoreCase("all")){
+                randMeat = "all meats";
+                total += .50;
+            }
+            if (randBean.equalsIgnoreCase("all")){
+                randBean = "all beans";
+                total += .50;
+            }
+            if (randSalsa.equalsIgnoreCase("all")){
+                randSalsa = "all salsas";
+                total += .50;
+            }
+            if (randVegge.equalsIgnoreCase("all")){
+                randVegge = "all veggies";
+                total += .50;
+            }
+            if (randSourCream.equalsIgnoreCase("yes")){
+                randSourCream = ", sour cream";
+                total += .50;
+            }
+            if (randGuac.equalsIgnoreCase("yes")){
+                randGuac = ", guacamole";
+                total += .50;
+            }
 
+            // output
+            System.out.println("Burrito " + i + ": " + randRice + ", " + randMeat +
+                    ", " + randBean + ", " + randSalsa + ", " + randVegge
+                    + randSourCream + randGuac + "\t$" + dec.format(total));
         }
 
     }
